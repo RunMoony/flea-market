734b582e911a6836e66bcb59c1b81a23612978f5
KwangJe Moon <iqboy963295@gmail.com>
2022-01-31 21:44:44 +0900

    페이지 디자인 작업

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,25 +1,23 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Community: NextPage = () => {
   return (
-    <div className='flex flex-col space-y-5 py-10'>
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className='flex px-4  border-b pb-5 cursor-pointer justify-between'
-        >
-          <div className='flex space-x-4'>
-            <div className='w-20 h-20 bg-gray-400 rounded-md' />
-            <div className='pt-2 flex flex-col'>
-              <h3 className='text-sm font-medium text-gray-900'>
-                New iPhone 14
-              </h3>
-              <span className='text-xs text-gray-500'>Black</span>
-              <span className='font-medium mt-1 text-gray-900'>$95</span>
-            </div>
+    <div className='py-10 space-y-8'>
+      {[1, 2, 3, 4, 5, 6].map((_, i) => (
+        <div key={i} className='flex cursor-pointer flex-col items-start'>
+          <span className='flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800'>
+            동네질문
+          </span>
+          <div className='mt-2 px-4 text-gray-700'>
+            <span className='text-orange-500 font-medium'>Q.</span> What is the
+            best mandu restaurant?
           </div>
-          <div className='flex space-x-2 items-end justify-end'>
-            <div className='flex space-x-0.5 items-center text-sm  text-gray-600'>
+          <div className='mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs'>
+            <span>니꼬</span>
+            <span>18시간 전</span>
+          </div>
+          <div className='flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full'>
+            <span className='flex space-x-2 items-center text-sm'>
               <svg
                 className='w-4 h-4'
                 fill='none'
@@ -31,12 +29,12 @@ const Home: NextPage = () => {
                   strokeLinecap='round'
                   strokeLinejoin='round'
                   strokeWidth='2'
-                  d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+                  d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                 ></path>
               </svg>
-              <span>1</span>
-            </div>
-            <div className='flex space-x-0.5 items-center text-sm  text-gray-600'>
+              <span>궁금해요 1</span>
+            </span>
+            <span className='flex space-x-2 items-center text-sm'>
               <svg
                 className='w-4 h-4'
                 fill='none'
@@ -51,30 +49,29 @@ const Home: NextPage = () => {
                   d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
                 ></path>
               </svg>
-              <span>1</span>
-            </div>
+              <span>답변 1</span>
+            </span>
           </div>
         </div>
       ))}
       <button className='fixed hover:bg-orange-500 transition-colors cursor-pointer  bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white'>
         <svg
-          className='h-6 w-6'
-          xmlns='http://www.w3.org/2000/svg'
+          className='w-6 h-6'
           fill='none'
-          viewBox='0 0 24 24'
           stroke='currentColor'
-          aria-hidden='true'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
         >
           <path
             strokeLinecap='round'
             strokeLinejoin='round'
             strokeWidth='2'
-            d='M12 6v6m0 0v6m0-6h6m-6 0H6'
-          />
+            d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'
+          ></path>
         </svg>
       </button>
     </div>
   );
 };
 
-export default Home;
+export default Community;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "media",
+  plugins: [require("@tailwindcss/forms")],
 };
