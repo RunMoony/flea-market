0c8a16b8465a9fa54a29b5af8ea519d4b8ff45c2
KwangJe Moon <iqboy963295@gmail.com>
2022-02-03 12:45:53 +0900

    refactoring

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,14 +22,9 @@ export default function Layout({
   };
   return (
     <div>
-      <div
-        className={cls(
-          !canGoBack ? "justify-center" : "",
-          "bg-white w-full max-w-xl text-lg px-10 font-medium py-3 fixed text-gray-800 border-b top-0  flex items-center"
-        )}
-      >
+      <div className='bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium  fixed text-gray-800 border-b top-0  flex items-center'>
         {canGoBack ? (
-          <button onClick={onClick}>
+          <button onClick={onClick} className='absolute left-4'>
             <svg
               className='w-6 h-6'
               fill='none'
@@ -46,13 +41,22 @@ export default function Layout({
             </svg>
           </button>
         ) : null}
-        {title ? <span>{title}</span> : null}
+        {title ? (
+          <span className={cls(canGoBack ? "mx-auto" : "", "")}>{title}</span>
+        ) : null}
       </div>
       <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
       {hasTabBar ? (
         <nav className='bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs'>
           <Link href='/'>
-            <a className='flex flex-col items-center space-y-2'>
+            <a
+              className={cls(
+                "flex flex-col items-center space-y-2 ",
+                router.pathname === "/"
+                  ? "text-orange-500"
+                  : "hover:text-gray-500 transition-colors"
+              )}
+            >
               <svg
                 className='w-6 h-6'
                 fill='none'
@@ -71,7 +75,14 @@ export default function Layout({
             </a>
           </Link>
           <Link href='/community'>
-            <a className='flex flex-col items-center space-y-2'>
+            <a
+              className={cls(
+                "flex flex-col items-center space-y-2 ",
+                router.pathname === "/community"
+                  ? "text-orange-500"
+                  : "hover:text-gray-500 transition-colors"
+              )}
+            >
               <svg
                 className='w-6 h-6'
                 fill='none'
@@ -90,7 +101,14 @@ export default function Layout({
             </a>
           </Link>
           <Link href='/chats'>
-            <a className='flex flex-col items-center space-y-2'>
+            <a
+              className={cls(
+                "flex flex-col items-center space-y-2 ",
+                router.pathname === "/chats"
+                  ? "text-orange-500"
+                  : "hover:text-gray-500 transition-colors"
+              )}
+            >
               <svg
                 className='w-6 h-6'
                 fill='none'
@@ -109,7 +127,14 @@ export default function Layout({
             </a>
           </Link>
           <Link href='/live'>
-            <a className='flex flex-col items-center space-y-2'>
+            <a
+              className={cls(
+                "flex flex-col items-center space-y-2 ",
+                router.pathname === "/live"
+                  ? "text-orange-500"
+                  : "hover:text-gray-500 transition-colors"
+              )}
+            >
               <svg
                 className='w-6 h-6'
                 fill='none'
@@ -128,7 +153,14 @@ export default function Layout({
             </a>
           </Link>
           <Link href='/profile'>
-            <a className='flex flex-col items-center space-y-2'>
+            <a
+              className={cls(
+                "flex flex-col items-center space-y-2 ",
+                router.pathname === "/profile"
+                  ? "text-orange-500"
+                  : "hover:text-gray-500 transition-colors"
+              )}
+            >
               <svg
                 className='w-6 h-6'
                 fill='none'
--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -1,84 +1,50 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
+import Message from "../../components/message";
 
-const Upload: NextPage = () => {
+const Stream: NextPage = () => {
   return (
-    <div className='px-4 space-y-5 py-10'>
-      <div>
-        <label className='w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md'>
-          <svg
-            className='h-12 w-12'
-            stroke='currentColor'
-            fill='none'
-            viewBox='0 0 48 48'
-            aria-hidden='true'
-          >
-            <path
-              d='M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02'
-              strokeWidth={2}
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
-          <input className='hidden' type='file' />
-        </label>
-      </div>
-      <div>
-        <label
-          className='mb-1 block text-sm font-medium text-gray-700'
-          htmlFor='name'
-        >
-          Name
-        </label>
-        <div className='rounded-md relative flex  items-center shadow-sm'>
-          <input
-            id='name'
-            type='email'
-            className='appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
-            required
-          />
+    <Layout canGoBack>
+      <div className='py-10 px-4  space-y-4'>
+        <div className='w-full rounded-md shadow-sm bg-slate-300 aspect-video' />
+        <div className='mt-5'>
+          <h1 className='text-3xl font-bold text-gray-900'>Galaxy S50</h1>
+          <span className='text-2xl block mt-3 text-gray-900'>$140</span>
+          <p className=' my-6 text-gray-700'>
+            My money&apos;s in that office, right? If she start giving me some
+            bullshit about it ain&apos;t there, and we got to go someplace else
+            and get it, I&apos;m gonna shoot you in the head then and there.
+            Then I&apos;m gonna shoot that bitch in the kneecaps, find out where
+            my goddamn money is. She gonna tell me too. Hey, look at me when
+            I&apos;m talking to you, motherfucker. You listen: we go in there,
+            and that ni**a Winston or anybody else is in there, you the first
+            motherfucker to get shot. You understand?
+          </p>
         </div>
-      </div>
-      <div>
-        <label
-          className='mb-1 block text-sm font-medium text-gray-700'
-          htmlFor='price'
-        >
-          Price
-        </label>
-        <div className='rounded-md relative flex  items-center shadow-sm'>
-          <div className='absolute left-0 pointer-events-none pl-3 flex items-center justify-center'>
-            <span className='text-gray-500 text-sm'>$</span>
+        <div>
+          <h2 className='text-2xl font-bold text-gray-900'>Live Chat</h2>
+          <div className='py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4'>
+            <Message message='Hi how much are you selling them for?' />
+            <Message message='I want ￦20,000' reversed />
+            <Message message='미쳤어' />
           </div>
-          <input
-            id='price'
-            className='appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500'
-            type='text'
-            placeholder='0.00'
-          />
-          <div className='absolute right-0 pointer-events-none pr-3 flex items-center'>
-            <span className='text-gray-500'>USD</span>
+          <div className='fixed py-2 bg-white  bottom-0 inset-x-0'>
+            <div className='flex relative max-w-md items-center  w-full mx-auto'>
+              <input
+                type='text'
+                className='shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500'
+              />
+              <div className='absolute inset-y-0 flex py-1.5 pr-1.5 right-0'>
+                <button className='flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white'>
+                  &rarr;
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <label
-          htmlFor='description'
-          className='mb-1 block text-sm font-medium text-gray-700'
-        >
-          Description
-        </label>
-
-        <textarea
-          id='description'
-          className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 '
-          rows={4}
-        />
-      </div>
-      <button className=' w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none '>
-        Upload item
-      </button>
-    </div>
+    </Layout>
   );
 };
 
-export default Upload;
+export default Stream;
