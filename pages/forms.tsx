e7792105a3651d619a4b874c0df7dc44d800d499
KwangJe Moon <iqboy963295@gmail.com>
2022-02-04 14:55:13 +0900

    useForm practice

---
+++ pages/forms.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onBlur",
+  });
+  const onValid = (data: LoginForm) => {
+    console.log("adsasdasd");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
+        type='text'
+        placeholder='Username'
+      />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type='email'
+        placeholder='Email'
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", { required: "Password is required" })}
+        type='password'
+        placeholder='Password'
+      />
+      <input type='submit' value='Create Account'></input>
+    </form>
+  );
+}
