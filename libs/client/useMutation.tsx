29cdad1c14d8bcf026723dfe5238685f481331c3
KwangJe Moon <iqboy963295@gmail.com>
2022-02-05 11:58:24 +0900

    login part

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,3 +1,28 @@
-export default function useMutation(url: string) {
-  function mutation(data: any) {}
+import { useState } from "react";
+
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+type UseMutationResult = [(data: any) => void, UseMutationState];
+export default function useMutation(url: string): UseMutationResult {
+  const [loading, setLoading] = useState<undefined | any>(false);
+  const [data, setData] = useState<undefined | any>(undefined);
+  const [error, setError] = useState<undefined | any>(undefined);
+  function mutation(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }
+  return [mutation, { loading, data, error }];
 }
