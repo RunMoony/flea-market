a0f9483c1fedf0e188aa60ad95933885870fa145
KwangJe Moon <iqboy963295@gmail.com>
2022-02-06 22:12:16 +0900

    Token logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,17 +6,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   /* if (email) {
     user = await client.user.findUnique({
       where: {
