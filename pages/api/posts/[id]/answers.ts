014bb6d52af6ab93be76aaffae59de048b3026e9
KwangJe Moon <iqboy963295@gmail.com>
2022-02-17 13:01:00 +0900

    동네생활 답변 기능 작업

---
+++ pages/api/posts/[id]/answers.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: +id.toString(),
+        },
+      },
+      answer,
+    },
+  });
+  res.json({
+    ok: true,
+    answer: newAnswer,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
