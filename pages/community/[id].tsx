a3143d091bb98037d3f900cebedb3671e0a76a7f
KwangJe Moon <iqboy963295@gmail.com>
2022-02-17 11:15:09 +0900

    동네질문 작업

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,23 +1,61 @@
-import { Post, User } from "@prisma/client";
+import TextArea from "@components/textarea";
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/client/utils";
+import { Answer, Post, User } from "@prisma/client";
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
+interface AnswerWithUser extends Answer {
+  user: User;
+}
+
 interface PostWithUser extends Post {
   user: User;
+  _count: {
+    answers: number;
+    wondering: number;
+  };
+  answers: AnswerWithUser[];
 }
 
 interface CommunityPostResponse {
   ok: boolean;
   post: PostWithUser;
+  isWondering: boolean;
 }
 
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
-  const { data, error } = useSWR<CommunityPostResponse>(
+  const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   );
+  const [wonder, { loading }] = useMutation(
+    `/api/posts/${router.query.id}/wonder`
+  );
+  const onWonderClick = () => {
+    if (!data) return;
+    mutate(
+      {
+        ...data,
+        post: {
+          ...data.post,
+          _count: {
+            ...data.post._count,
+            wondering: data.isWondering
+              ? data?.post._count.wondering - 1
+              : data?.post._count.wondering + 1,
+          },
+        },
+        isWondering: !data.isWondering,
+      },
+      false
+    );
+    if (!loading) {
+      wonder({});
+    }
+  };
   return (
     <div>
       <span className='inline-flex my-3 ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800'>
@@ -27,9 +65,9 @@ const CommunityPostDetail: NextPage = () => {
         <div className='w-10 h-10 rounded-full bg-slate-300' />
         <div>
           <p className='text-sm font-medium text-gray-700'>
-            {data?.post?.user.name}
+            {data?.post?.user?.name}
           </p>
-          <Link href={`/users/profiles/${data?.post?.user?.id}`}>
+          <Link href={`/users/profiles/${data?.post.user.id}`}>
             <a className='text-xs font-medium text-gray-500'>
               View profile &rarr;
             </a>
@@ -42,7 +80,13 @@ const CommunityPostDetail: NextPage = () => {
           {data?.post?.question}
         </div>
         <div className='flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full'>
-          <span className='flex space-x-2 items-center text-sm'>
+          <button
+            onClick={onWonderClick}
+            className={cls(
+              "flex space-x-2 items-center text-sm",
+              data?.isWondering ? "text-teal-400" : ""
+            )}
+          >
             <svg
               className='w-4 h-4'
               fill='none'
@@ -58,7 +102,7 @@ const CommunityPostDetail: NextPage = () => {
               ></path>
             </svg>
             <span>궁금해요</span>
-          </span>
+          </button>
           <span className='flex space-x-2 items-center text-sm'>
             <svg
               className='w-4 h-4'
@@ -79,25 +123,26 @@ const CommunityPostDetail: NextPage = () => {
         </div>
       </div>
       <div className='px-4 my-5 space-y-5'>
-        <div className='flex items-start space-x-3'>
-          <div className='w-8 h-8 bg-slate-200 rounded-full' />
-          <div>
-            <span className='text-sm block font-medium text-gray-700'>
-              Steve Jebs
-            </span>
-            <span className='text-xs text-gray-500 block '>2시간 전</span>
-            <p className='text-gray-700 mt-2'>
-              The best mandu restaurant is the one next to my house.
-            </p>
+        {data?.post?.answers?.map((answer) => (
+          <div key={answer.id} className='flex items-start space-x-3'>
+            <div className='w-8 h-8 bg-slate-200 rounded-full' />
+            <div>
+              <span className='text-sm block font-medium text-gray-700'>
+                {answer.user.name}
+              </span>
+              <span className='text-xs text-gray-500 block '>
+                {answer.createdAt}
+              </span>
+              <p className='text-gray-700 mt-2'>{answer.answer}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
       <div className='px-4'>
-        <textarea
-          className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 '
-          rows={4}
+        <TextArea
+          name='description'
           placeholder='Answer this question!'
-        />
+        ></TextArea>
         <button className='mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none '>
           Reply
         </button>
