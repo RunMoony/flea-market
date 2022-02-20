6acfc5dbebd1c584b401a771b464fadbc1709211
KwangJe Moon <iqboy963295@gmail.com>
2022-02-20 12:47:51 +0900

    나의 캐럿 -> 판매내역 작업

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,19 +1,31 @@
 import type { NextPage } from "next";
 import Item from "components/item";
 import Layout from "components/layout";
+import useSWR from "swr";
+import { ProductWithCount } from "pages";
+
+interface Record {
+  id: number;
+  product: ProductWithCount;
+}
+
+interface ProductListResponse {
+  [key: string]: Record[];
+}
 
 const Sold: NextPage = () => {
+  const { data } = useSWR<ProductListResponse>(`/api/users/me/sales`);
   return (
     <Layout title='판매내역' canGoBack>
       <div className='flex flex-col space-y-5 pb-10  divide-y'>
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        {data?.sales?.map((record) => (
           <Item
-            id={i}
-            key={i}
-            title='iPhone 14'
-            price={99}
+            id={record.product.id}
+            key={record.id}
+            title={record.product.name}
+            price={record.product.price}
             comments={1}
-            hearts={1}
+            hearts={record.product._count.favs}
           />
         ))}
       </div>
