7ff517b29de21e8982a36c79be5167e206939065
KwangJe Moon <iqboy963295@gmail.com>
2022-02-20 19:23:10 +0900

    refactoring

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,33 +1,13 @@
 import type { NextPage } from "next";
 import Item from "components/item";
 import Layout from "components/layout";
-import useSWR from "swr";
-import { ProductWithCount } from "pages";
-
-interface Record {
-  id: number;
-  product: ProductWithCount;
-}
-
-interface ProductListResponse {
-  [key: string]: Record[];
-}
+import ProductList from "@components/product-list";
 
 const Sold: NextPage = () => {
-  const { data } = useSWR<ProductListResponse>(`/api/users/me/sales`);
   return (
     <Layout title='판매내역' canGoBack>
       <div className='flex flex-col space-y-5 pb-10  divide-y'>
-        {data?.sales?.map((record) => (
-          <Item
-            id={record.product.id}
-            key={record.id}
-            title={record.product.name}
-            price={record.product.price}
-            comments={1}
-            hearts={record.product._count.favs}
-          />
-        ))}
+        <ProductList kind='sales' />
       </div>
     </Layout>
   );
