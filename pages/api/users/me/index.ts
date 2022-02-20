d82bb977ab91f9fba051a58b4546104ece29b5f3
KwangJe Moon <iqboy963295@gmail.com>
2022-02-20 21:14:25 +0900

    계정관리 작업

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
+import { json } from "stream/consumers";
 
 declare module "iron-session" {
   interface IronSessionData {
@@ -24,11 +25,89 @@ async function handler(
       profile,
     });
   }
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { email, phone, name },
+    } = req;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+    if (email && email !== currentUser?.email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Email already taken",
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        },
+      });
+      res.json({ ok: true });
+    }
+    if (phone && phone !== currentUser?.phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Phone already in use.",
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        },
+      });
+      res.json({ ok: true });
+    }
+    if (name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+    }
+    res.json({
+      ok: true,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET"],
+    methods: ["GET", "POST"],
     handler,
   })
 );
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,61 @@ import type { NextPage } from "next";
 import Button from "components/button";
 import Input from "components/input";
 import Layout from "components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import useMutation from "@libs/client/useMutation";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  name?: string;
+  formErrors?: string;
+}
+
+interface EditProfileResponse {
+  ok: boolean;
+  error?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.name) setValue("name", user.name);
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+  const [editProfile, { data, loading }] =
+    useMutation<EditProfileResponse>(`/api/users/me`);
+  const onValid = ({ email, phone, name }: EditProfileForm) => {
+    if (loading) return;
+    if (email === "" && phone === "" && name === "") {
+      return setError("formErrors", {
+        message: "Email 또는 전화번호를 입력해주세요!",
+      });
+    }
+    editProfile({
+      email,
+      phone,
+      name,
+    });
+  };
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data, setError]);
+
   return (
     <Layout canGoBack title='Edit Profile'>
-      <form className='py-10 px-4 space-y-4'>
+      <form onSubmit={handleSubmit(onValid)} className='py-10 px-4 space-y-4'>
         <div className='flex items-center space-x-3'>
           <div className='w-14 h-14 rounded-full bg-slate-500' />
           <label
@@ -22,15 +72,34 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label='Email address' name='email' type='email' />
         <Input
-          required
+          register={register("name")}
+          required={false}
+          label='Name'
+          name='name'
+          type='text'
+        />
+        <Input
+          register={register("email")}
+          required={false}
+          label='Email address'
+          name='email'
+          type='email'
+        />
+        <Input
+          register={register("phone")}
+          required={false}
           label='Phone number'
           name='phone'
           type='number'
           kind='phone'
         />
-        <Button text='Update profile' />
+        {errors.formErrors ? (
+          <span className='my-2 text-red-500 font-medium text-center block'>
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text={loading ? "Loading..." : "Update profile"} />
       </form>
     </Layout>
   );
