2bec4bb5270e83afc1d63fe58e7ff1d008e07e89
KwangJe Moon <iqboy963295@gmail.com>
2022-02-21 19:36:02 +0900

    mutate 사용하여 실시간채팅 성능 개선

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -37,7 +37,10 @@ const Stream: NextPage = () => {
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   console.log(register);
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000,
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -45,13 +48,30 @@ const Stream: NextPage = () => {
   const onValid = (form: MessageForm) => {
     if (loading) return null;
     reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream.messages,
+              {
+                id: Date.now(),
+                message: form.message,
+                user: {
+                  ...user,
+                },
+              },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
+
   return (
     <Layout canGoBack>
       <div className='py-10 px-4  space-y-4'>
